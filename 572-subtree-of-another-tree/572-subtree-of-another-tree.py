2ab3b4ec74757efdb3486c09ec626b7b29c4f3d2
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2022-09-19 10:43:42 -0400

    Time: 305 ms (9.81%), Space: 15 MB (64.19%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -5,35 +5,57 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode], checkingSubtree=False) -> bool:
-        if root is None and subRoot is None:
-            return True
-        elif not root or not subRoot:
-            return False
+        if not subRoot: return True
+        if not root: return False
         
-        if root.val == subRoot.val:
-            root_same = self.checkEquality(root, subRoot)
+        if root.val == subRoot.val and self.sameTree(root, subRoot):
+            return True
         else:
-            root_same = False
+            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        
+    
+    def sameTree(self, s, t):
+        if not s and not t: return True
+        if not s or not t: return False
+        
+        if s.val == t.val: return self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
+        else:
+            return False
+    
+    
+    
+    
+#     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode], checkingSubtree=False) -> bool:
+#         if root is None and subRoot is None:
+#             return True
+#         elif not root or not subRoot:
+#             return False
+        
+#         if root.val == subRoot.val:
+#             root_same = self.checkEquality(root, subRoot)
+#         else:
+#             root_same = False
             
-        l_same = self.isSubtree(root.left, subRoot)
-        r_same = self.isSubtree(root.right, subRoot)
+#         l_same = self.isSubtree(root.left, subRoot)
+#         r_same = self.isSubtree(root.right, subRoot)
         
-        return l_same or r_same or root_same
+#         return l_same or r_same or root_same
         
         
-    def checkEquality(self, root, subRoot):
-        if not root and not subRoot:
-            return True
-        elif not root or not subRoot:
-            return False
-        elif root.val != subRoot.val:
-            return False
+#     def checkEquality(self, root, subRoot):
+#         if not root and not subRoot:
+#             return True
+#         elif not root or not subRoot:
+#             return False
+#         elif root.val != subRoot.val:
+#             return False
         
-        l_same = self.checkEquality(root.left, subRoot.left)
-        r_same = self.checkEquality(root.right, subRoot.right)
+#         l_same = self.checkEquality(root.left, subRoot.left)
+#         r_same = self.checkEquality(root.right, subRoot.right)
         
-        return l_same and r_same
+#         return l_same and r_same
         
         
             
