288a35b15c9d13d20dfb47d4bb67d49e74386ba2
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2022-09-19 11:35:57 -0400

    Time: 238 ms (17.19%), Space: 18.7 MB (71.66%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,7 +1,8 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
+import heapq
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent2(self, nums: List[int], k: int) -> List[int]:
         hashMapFreqLetter = defaultdict(list)
         hashMapLetterFreq = defaultdict(int)
         for n in nums:
@@ -17,7 +18,12 @@ class Solution:
                 output.append(n)
                 if len(output) == k:
                     return output
-
+    
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if len(nums) == k: return nums
+        counter = Counter(nums)
+        return heapq.nlargest(k, counter.keys(), key=counter.get)
+        
                 
             
             
