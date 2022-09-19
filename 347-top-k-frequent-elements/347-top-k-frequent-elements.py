c2b77ade764145921234313a57d1cf948c49ec8f
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2022-09-19 11:29:40 -0400

    Time: 207 ms (30.55%), Space: 18.6 MB (71.66%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -2,18 +2,18 @@ from collections import defaultdict
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashMapFreq = defaultdict(list)
-        hashMapLetter = defaultdict(int)
+        hashMapFreqLetter = defaultdict(list)
+        hashMapLetterFreq = defaultdict(int)
         for n in nums:
-            hashMapLetter[n] += 1
-            if hashMapLetter[n] > 1:
-                hashMapFreq[hashMapLetter[n]-1].remove(n)
-            hashMapFreq[hashMapLetter[n]].append(n)
+            hashMapLetterFreq[n] += 1
+            if hashMapLetterFreq[n] > 1:
+                hashMapFreqLetter[hashMapLetterFreq[n]-1].remove(n)
+            hashMapFreqLetter[hashMapLetterFreq[n]].append(n)
             
-        freqKeys = list(hashMapFreq.keys())
+        freqKeys = list(hashMapFreqLetter.keys())
         output = []
         for key in freqKeys[::-1]:
-            for n in hashMapFreq[key]:
+            for n in hashMapFreqLetter[key]:
                 output.append(n)
                 if len(output) == k:
                     return output
