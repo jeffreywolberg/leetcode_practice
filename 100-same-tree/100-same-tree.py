3d14aa11be9ddb437e9da9560a09f820021af603
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2022-09-19 09:33:18 -0400

    Time: 33 ms (91.64%), Space: 13.8 MB (75.60%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -9,7 +9,7 @@ class Solution:
         if p is None and q is None:
             return True
         
-        if (p is None and q is not None) or (p is not None and q is None):
+        elif p is None or q is None:
             return False
         
         elif p.val != q.val:
