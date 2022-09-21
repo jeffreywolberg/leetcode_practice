f6809315f0cad6aec324fbc73509fae461a9e99c
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2022-09-21 10:30:21 -0400

    Time: 110 ms (18.96%), Space: 15.5 MB (20.07%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -4,8 +4,8 @@ import string
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        print(s)
-        s = s.translate(str.maketrans('', '', string.punctuation+' '))
+        isalnum = lambda s: s.isalnum()
+        s = list(filter(isalnum, s))
         i = 0
         j = len(s) - 1
         while(i < j):
