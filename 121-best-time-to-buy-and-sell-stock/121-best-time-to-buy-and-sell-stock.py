eabcb17733c25a332617872d421db44305d61753
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2022-09-23 15:16:02 -0400

    Time: 2515 ms (15.62%), Space: 24.9 MB (94.82%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         maxProf = 0
-        minP = math.inf
-        
-        for i in range(len(prices)):
-            if prices[i] < minP:
-                minP = prices[i]
-            elif prices[i] - minP > maxProf:
-                maxProf = prices[i] - minP
+        left = 0
+        for i, p in enumerate(prices):
+            if p-prices[left] > maxProf:
+                maxProf = p-prices[left]
+            if p < prices[left]:
+                left = i
             
         return maxProf
-            
