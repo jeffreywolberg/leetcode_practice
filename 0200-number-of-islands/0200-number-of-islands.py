4c4b2c5cc6ed22c45891159e84857e9e6c8fe451
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2023-11-28 00:10:35 -0500

    Time: 287 ms (62.11%), Space: 18.7 MB (80.08%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -12,12 +12,35 @@ class Solution:
         self.dfs(grid, y-1, x) # up
         self.dfs(grid, y, x-1) # left
         self.dfs(grid, y+1, x) # up
+    
+    def bfs(self, grid, y, x):
+        queue = [(y,x)]
+        while queue:
+            y, x = queue.pop(0)
+            if y < 0 or y >= len(grid) or x < 0 or x >= len(grid[0]):
+                continue
+            elif grid[y][x] == "1":
+                grid[y][x] = "0"
+                queue.append((y, x+1))
+                queue.append((y-1, x))
+                queue.append((y, x-1))
+                queue.append((y+1, x))
+        
+            
         
     def numIslands(self, grid: List[List[str]]) -> int:
+        # numIslands = 0
+        # for y in range(len(grid)):
+        #     for x in range(len(grid[0])):
+        #         if grid[y][x] == "1":
+        #             self.dfs(grid, y, x)
+        #             numIslands += 1
+        # return numIslands
+    
         numIslands = 0
         for y in range(len(grid)):
             for x in range(len(grid[0])):
                 if grid[y][x] == "1":
-                    self.dfs(grid, y, x)
+                    self.bfs(grid, y, x)
                     numIslands += 1
         return numIslands
