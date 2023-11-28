99011a885b8dc82fd2e33cde10e13f8036de29c4
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2023-11-27 23:03:34 -0500

    Time: 96 ms (71.88%), Space: 21.1 MB (40.57%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,13 +2,16 @@ from collections import Counter, defaultdict
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        bucket = [[] for _ in range(len(nums)+1)]
-        count = Counter(nums)
-        for num, freq in count.items():
-            bucket[freq].append(num)
-        
-        print(bucket)
-        flat_list = [item for sublist in bucket for item in sublist]
+        """v3: bucket sort"""
+        element_freq = {}  # Dict[element, freq]
+        for n in nums:
+            element_freq[n] = element_freq.get(n, 0) + 1
+
+        buckets = [[] for _ in range(max(element_freq.values()) + 1)]
+        for n, freq in element_freq.items():
+            buckets[freq].append(n)
+        flat_list = [n for bucket in buckets for n in bucket]
+
         return flat_list[-k:]
         
         
